52e8aff6534c14611ca2114c2da4b5267c66aa48
akiogitgit <akimaron2828@icloud.com>
2022-10-28 11:11:56 +0900

    feat: ディレクトリ構造修正

---
+++ src/pages/progate.tsx
@@ -0,0 +1,23 @@
+import { NextPage } from 'next'
+
+const Progate: NextPage = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+          <div>アイコン</div>
+          <div>コース一覧</div>
+          <div>スライド検索</div>
+          <div>ランキング</div>
+          <div>ヘルプ</div>
+        </nav>
+        <div>
+          <button>Akio (Lv.176)</button>
+          <div>通知</div>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Progate
