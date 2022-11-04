18338f11d623113bf557bfdfff2d1ba7e5e244c5
akiogitgit <akimaron2828@icloud.com>
2022-11-04 14:10:09 +0900

    chore: indexページに移動

---
+++ src/pages/emotion-test.tsx
@@ -0,0 +1,44 @@
+import { NextPage } from 'next'
+import { css } from '@emotion/react'
+
+const EmotionDemo: NextPage = () => {
+  return (
+    <>
+      <h1 css={title}>Emotion</h1>
+      <div css={[{ fontSize: '200px' }, title]}>aaa</div>
+      <button css={button('blue')}>
+        Button<div>anpan</div>
+      </button>
+    </>
+  )
+}
+
+export default EmotionDemo
+
+const title = css({
+  color: 'red',
+  marginLeft: '130px',
+  '&:hover': {
+    color: 'white',
+  },
+})
+
+const button = (color: 'red' | 'blue' | 'green' = 'red') =>
+  css({
+    outline: 'none',
+    padding: '5px 10px',
+    background: color,
+    border: `${color} 1px solid`,
+    borderRadius: '100px',
+    color: 'white',
+    fontWeight: 700,
+    cursor: 'pointer',
+    transition: 'all 300ms',
+    '&:hover': {
+      background: 'white',
+      color: color,
+    },
+    '& div': {
+      margin: '20px 0',
+    },
+  })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,24 @@
 import { NextPage } from 'next'
 import { css } from '@emotion/react'
+import { Layout } from '../components/Layout'
+import { NewContents } from '../components/sections/NewContents'
+import { WebDevelopmentCourse } from '../components/sections/WebDevelopmentCourse'
+import { SectionWrapper } from '../components/SectionWrapper'
+import { LanguageCourse } from '../components/sections/LanguageCourse'
 
-const Index: NextPage = () => {
+const Progate: NextPage = () => {
   return (
-    <>
-      <h1 css={title}>Emotion</h1>
-      <div css={[{ fontSize: '200px' }, title]}>aaa</div>
-      <button css={button('blue')}>
-        Button<div>anpan</div>
-      </button>
-    </>
+    <Layout>
+      <NewContents />
+      <SectionWrapper>
+        <WebDevelopmentCourse />
+      </SectionWrapper>
+
+      <SectionWrapper>
+        <LanguageCourse />
+      </SectionWrapper>
+    </Layout>
   )
 }
 
-export default Index
-
-const title = css({
-  color: 'red',
-  marginLeft: '130px',
-  '&:hover': {
-    color: 'white',
-  },
-})
-
-const button = (color: 'red' | 'blue' | 'green' = 'red') =>
-  css({
-    outline: 'none',
-    padding: '5px 10px',
-    background: color,
-    border: `${color} 1px solid`,
-    borderRadius: '100px',
-    color: 'white',
-    fontWeight: 700,
-    cursor: 'pointer',
-    transition: 'all 300ms',
-    '&:hover': {
-      background: 'white',
-      color: color,
-    },
-    '& div': {
-      margin: '20px 0',
-    },
-  })
+export default Progate
